87ab50f1f8c5272c8f5e6b2d6476329ff41717d4
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-05-08 00:47:50 +0300

    Создание javadoc

---
+++ chapter_001/src/main/java/ru/mmm/calculate/Calculate.java
@@ -0,0 +1,23 @@
+package ru.mmm.calculate;
+
+   /**
+    * Class Класс калкулятор
+    * @author giorgyi nishnianidze
+    * @since 08.05.2018
+    * @version : 1.0
+    */
+
+public class Calculate {
+
+   /**
+    * Main.
+    * @param args - args.
+    */
+
+    public static void main(String[] args) {
+
+          System.out.println("Hello World");
+
+       }
+
+}
---
+++ chapter_001/src/main/java/ru/mmm/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Georgyi Nishnianidze (greserve12@yandex.ru)
+* @version $Id$
+* @since 08.05.2018
+*/
+package ru.mmm.calculate;
+ 
