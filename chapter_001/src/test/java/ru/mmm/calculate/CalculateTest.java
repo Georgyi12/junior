a56284162ef84e38b6e583b4c9b9b5c045762386
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-05-17 23:59:07 +0300

    Скриншот

---
+++ chapter_001/src/test/java/ru/mmm/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Georgyi Nishnianidze 
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/ @Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Georgyi Nishnianidze";
+    String expect = "Echo, echo, echo : Georgyi Nishnianidzea";
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
+ 
