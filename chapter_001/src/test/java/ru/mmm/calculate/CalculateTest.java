5b674a8dab43ca0e7bf732f3d0493d2c8dd648ab
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-06-01 15:31:43 +0300

    Использование Git в IDEA [#31170]

--- chapter_001/src/test/java/ru/mmm/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/mmm/calculate/CalculateTest.java
@@ -5,23 +5,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Georgyi Nishnianidze 
-* @version $Id$
-* @since 0.1
-*/
-   public class CalculateTest {
-/**
-* Test echo.
-*/ @Test
+ * Test.
+ *
+ * @author Georgyi Nishnianidze
+ * @version $Id$
+ * @since 0.1
+ */
+public class CalculateTest {
+   /**
+    * Test echo.
+    */ @Test
    public void  whenTakeNameThenTreeEchoPlusName() {
-    String input = "Georgyi Nishnianidze";
-    String expect = "Echo, echo, echo :Georgyi Nishnianidze";
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
+      String input = "Georgyi Nishnianidze";
+      String expect = "Echo, echo, echo :Georgyi Nishnianidze";
+      Calculate calc = new Calculate();
+      String result = calc.echo(input);
+      assertThat(result, is(expect));
+   }
 
 }
- 
