9710e0267590f63b2b3390ec68c531a1b7128d68
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-06-09 00:43:38 +0300

    Конвертер валюты. [#41583]

---
+++ chapter_001/src/main/java/ru/mmm/converter/Converter.java
@@ -0,0 +1,47 @@
+package ru.mmm.converter;
+
+/**
+ * Корвертор валюты.
+ */
+public class Converter {
+
+    /**
+     * Конвертируем рубли в евро.
+     * @param value рубли.
+     * @return Евро.
+     */
+    public int rubleToEuro(int value) {
+        return 1;
+    }
+
+    /**
+     * Конвертируем рубли в доллары.
+     * @param value рубли.
+     * @return Доллары
+     */
+    public int rubleToDollar(int value) {
+        return 1;
+
+    }
+
+    /**
+     * Конвертируем евро в рубли.
+     * @param value рубли.
+     * @return рубль
+     */
+    public int euroToRouble(int value) {
+       return 1;
+    }
+
+    /**
+     * Конвертируем доллар в рубли.
+     * @param value рубли.
+     * @return рубль
+     */
+    public int dollarToRouble(int value) {
+
+        return 1;
+    }
+
+}
+
---
+++ chapter_001/src/test/java/ru/mmm/converter/ConverterTest.java
@@ -0,0 +1,37 @@
+package ru.mmm.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConverterTest {
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void when1DollarToRoubleThen60() {
+        Converter converter = new Converter();
+        int result = converter.dollarToRouble(1);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void when1EuroToRoubleThen70() {
+        Converter converter = new Converter();
+        int result = converter.euroToRouble(1);
+        assertThat(result, is(1));
+    }
+
+
+}
