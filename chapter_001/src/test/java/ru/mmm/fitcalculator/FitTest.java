a70a2e83743cc9120e9f58d2bb9b846b633cb1ae
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-06-09 22:26:58 +0300

    Идеальный вес. [#41584]

---
+++ chapter_001/src/test/java/ru/mmm/fitcalculator/FitTest.java
@@ -0,0 +1,23 @@
+package ru.mmm.fitcalculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+public class FitTest {
+
+    @Test
+    public void manWeight() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(180);
+        assertThat(weight, closeTo(65.0, 0.1));
+    }
+
+    @Test
+    public void womanWeight() {
+        Fit fit = new Fit();
+        double weight = fit.womanWeight(170);
+        assertThat(weight, closeTo(43.5, 0.1));
+    }
+}
