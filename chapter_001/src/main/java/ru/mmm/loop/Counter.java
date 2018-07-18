792e7080a7c40a2b26015c029d8be81f69f59113
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-07-19 00:33:04 +0300

    counter2

--- chapter_001/src/main/java/ru/mmm/loop/Counter.java
+++ chapter_001/src/main/java/ru/mmm/loop/Counter.java
@@ -4,14 +4,13 @@ public class Counter {
 
     public int add(int start, int finish    ) {
 
-        double x = 0;
+        int result = 0;
         int i = 0;
-        for (i = 0; start <= finish; i++) {
-            if (start % 2 == 0) {
-                // x = 0;
-                x = x + start;
+        for (i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+
             }
         }
-      return (int) x;
+      return result;
     }
 }
