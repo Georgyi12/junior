ca9c6cc46483bab52f577ef697d76b45c5ea7df2
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-08-03 23:23:03 +0300

    шахматная доска в псевдографике2

--- chapter_001/src/main/java/ru/mmm/loop/Paint.java
+++ chapter_001/src/main/java/ru/mmm/loop/Paint.java
@@ -6,27 +6,19 @@ package ru.mmm.loop;
  * @since 0.1
  */
 public class Paint {
-    public String rightTrl(int height) {
-        // Буфер для результата.
+    public String leftTrl(int height) {
         StringBuilder screen = new StringBuilder();
-        // ширина будет равна высоте.
         int weight = height;
-        // внешний цикл двигается по строкам.
         for (int row = 0; row != height; row++) {
-            // внутренний цикл определяет положение ячейки в строке.
             for (int column = 0; column != weight; column++) {
-                // если строка равна ячейки, то рисуем галку.
-                // в данном случае строка определяем, сколько галок будет на строке
-                if (row >= column) {
+                if (row >= weight - column - 1) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
                 }
             }
-            // добавляем перевод строки.
             screen.append(System.lineSeparator());
         }
-        // Получаем результат.
         return screen.toString();
     }
 }
