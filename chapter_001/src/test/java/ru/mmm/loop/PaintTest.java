6c4d685a97140e9e2ef0362a6454675a31c9a884
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-08-04 16:28:09 +0300

    Пирамиды в псевдографике

--- chapter_001/src/test/java/ru/mmm/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/mmm/loop/PaintTest.java
@@ -5,26 +5,58 @@ import java.util.StringJoiner;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
-/**
- * @author Petr Arsentev (mailto:parsentev@yandex.ru)
- * @version $Id$
- * @since 0.1
- */
+
 public class PaintTest {
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(),
+                "", System.lineSeparator())
+                .add("^   ")
+                .add("^^  ")
+                .add("^^^ ")
+                .add("^^^^")
+                .toString()
+        )
+        );
+
+    }
+
     @Test
     public void whenPyramid4Left() {
         Paint paint = new Paint();
         String rst = paint.leftTrl(4);
         System.out.println(rst);
-        assertThat(rst,
-                is(
-                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
-                                .add("   ^")
-                                .add("  ^^")
-                                .add(" ^^^")
-                                .add("^^^^")
-                                .toString()
-                )
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(),
+                "", System.lineSeparator())
+                .add("   ^")
+                .add("  ^^")
+                .add(" ^^^")
+                .add("^^^^")
+                .toString()
+        )
+        );
+    }
+
+
+    @Test
+    public void whenPyramid4Pyramid() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(),
+                "", System.lineSeparator())
+                .add("   ^   ")
+                .add("  ^^^  ")
+                .add(" ^^^^^ ")
+                .add("^^^^^^^")
+                .toString()
+        )
         );
     }
 }
+
+
+
