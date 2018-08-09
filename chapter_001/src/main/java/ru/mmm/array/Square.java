0ff5123c04f33ce157aadefb103c0301b209b278
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-08-09 21:44:29 +0300

    Заполнение массива числами возведенными в квадрат

--- chapter_001/src/main/java/ru/mmm/array/Square.java
+++ chapter_001/src/main/java/ru/mmm/array/Square.java
@@ -4,14 +4,11 @@ public class Square {
     public int[] calculate(int bound) {
         int[] rst = new int[bound];
         // заполнить массив через цикл элементами от 1 до bound возведенными в квадрат
-        for (int i = 1; i < rst.length; i++) {
+        for (int i = 0; i < rst.length; i++) {
 
-             rst[i] = i * i;
+            rst[i] = (i + 1)* (i + 1);
+
         }
-
-
-
-
 
         return rst;
     }
