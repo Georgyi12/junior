127e8b108dba59adc8fe73053477b653934fcb9b
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-08-17 16:53:39 +0300

    Массив заполнен true или false3

--- chapter_001/src/main/java/ru/mmm/array/Check.java
+++ chapter_001/src/main/java/ru/mmm/array/Check.java
@@ -2,12 +2,10 @@ package ru.mmm.array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
+        boolean result = true;
         for (int index = 0; index < data.length; index++) {
-            if (data[0] == data[index]) {
-                result = true;
-            }
-            else if (data[0] != data[index]) {
+
+             if (data[0] != data[index]) {
                 result = false;
                 break;
             }
