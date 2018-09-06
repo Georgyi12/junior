4955c2923ce6806986050a9c38c3fe9cc0fd43f8
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-09-06 13:44:31 +0300

    Квадратный массив заполнен true или false по диагоналям

---
+++ chapter_001/src/main/java/ru/mmm/array/MatrixCheck.java
@@ -0,0 +1,20 @@
+package ru.mmm.array;
+
+public class MatrixCheck {
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        for (int i = 0; i < data.length; i++) {
+            if (data[0][0] != data[i][i]) {
+                result = false;
+                break;
+            }
+
+        }
+        return result;
+    }
+}
+
+
+
+
+
