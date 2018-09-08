d38a488f600985e972a2ba5cbad5ba7a9a68c442
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-09-08 15:38:12 +0300

    Удаление дубликатов в массиве1

---
+++ chapter_001/src/test/java/ru/mmm/array/ArrayDuplicateTest.java
@@ -0,0 +1,21 @@
+package ru.mmm.array;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        //напишите здесь тест, проверяющий удаление дубликатов строк из массива строк.
+        String[] input = {"Liza", "Olga", "Liza", "Sveta", "Valeria"};
+        String[] except = {"Liza", "Olga", "Sveta", "Valeria"};
+
+
+
+    }
+}
+
